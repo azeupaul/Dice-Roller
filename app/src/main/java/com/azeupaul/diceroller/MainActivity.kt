934347bd9557a8bae03fa09a053b635a1c2daeae
azeupaul <azeupaul@gmail.com>
2021-06-27 20:39:11 +0100

    Add button and change text in activity class

--- app/src/main/java/com/azeupaul/diceroller/MainActivity.kt
+++ app/src/main/java/com/azeupaul/diceroller/MainActivity.kt
@@ -2,10 +2,13 @@ package com.azeupaul.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val roll_button: Button = findViewById(R.id.roll_button)
+        roll_button.text = "Let's roll"
     }
 }
